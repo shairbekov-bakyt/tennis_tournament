c9fbbd2e8750d1c05f64465e5ad434b50b999032
c0mrade <ShairbekovBakyt@gmail.com>
2022-07-23 01:45:48 +0600

    code_refactor

--- backend/tennis/views.py
+++ backend/tennis/views.py
@@ -11,8 +11,11 @@ class GameViewSet(ModelViewSet):
     def get_serializer_class(self):
         if self.action == 'list':
             return GameSerializerGet
+
         if self.action == 'create':
             return GameSerializerPost
+
         if self.action == 'update':
             return GameSerializerUpdate
+
         return GameSerializerGet
